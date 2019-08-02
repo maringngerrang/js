77c5238f50891bb2c995cb3845d0fb531326703c
Maringngerrang Blog <maringngerrang@gmail.com>
2019-08-03 03:07:03 +0800

    Create chatbook-r.js

---
+++ chatbook-r.js
@@ -0,0 +1,1 @@
+var a=document.getElementById("gbcredit"),e=document.getElementById("gb"),s=e.offsetWidth,t="-"+s+"px";function gbtoggle(){e.style.right<=t?e.style.right="0":e.style.right=t}e.style.right=t,a.href="https://www.maringngerrang.com/2015/12/membuat-buku-tamu-blog.html",a.innerHTML="Get now in Maringngerrang!";
